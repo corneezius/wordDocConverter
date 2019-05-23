71ca758aa711653052ca6ab124c9d30b2917de10
Corneezius <cfr2116@columbia.edu>
2019-05-22 21:59:00 -0400

    build word doc to base64 converter

---
+++ app.js
@@ -0,0 +1,38 @@
+var fs = require("fs");
+var express = require("express");
+var bodyParser = require('body-parser')
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
+
+function base64_encode(file) {
+    // read binary data
+    var bitmap = fs.readFileSync(file);
+    // convert binary data to base64 encoded string
+    return new Buffer(bitmap).toString('base64');
+}
+
+app.get("/", (req, res, next) => {
+ res.send("Retrieves Word App Document Base64");
+});
+
+app.get("/word", (req, res, next) => {
+  res.send(base64_encode("./wordApp2.docx"));
+})
+
+app.listen(3000, () => {
+ console.log("Server running on port 3000");
+});
